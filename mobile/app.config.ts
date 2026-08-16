0450f338b71430c052e92ed7849f88da8b5b73cf
Hassoun911 <65694110+Hassoun911@users.noreply.github.com>
2026-08-16 11:02:53 -0400

    Bump WOPT Android app to modern UI v0.2.0

--- mobile/app.config.ts
+++ mobile/app.config.ts
@@ -5,7 +5,7 @@ const config: ExpoConfig = {
   slug: "wopt",
   owner: "hassoun911",
   scheme: "windsorprayer",
-  version: "0.1.0",
+  version: "0.2.0",
   orientation: "portrait",
   icon: "./assets/icon.png",
   userInterfaceStyle: "automatic",
@@ -19,6 +19,7 @@ const config: ExpoConfig = {
   },
   android: {
     package: "ca.wopt.windsorprayertimes",
+    versionCode: 2,
     adaptiveIcon: {
       foregroundImage: "./assets/adaptive-icon.png",
       backgroundColor: "#0b5b47"
