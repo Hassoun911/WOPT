cb9d3f48ea77dd69b3306bb614b93394d6efa6a1
Hassoun911 <65694110+Hassoun911@users.noreply.github.com>
2026-08-16 13:10:04 -0400

    Bump Android Quran fix to v0.3.1

--- mobile/app.config.ts
+++ mobile/app.config.ts
@@ -5,7 +5,7 @@ const config: ExpoConfig = {
   slug: "wopt",
   owner: "hassoun911",
   scheme: "windsorprayer",
-  version: "0.3.0",
+  version: "0.3.1",
   orientation: "portrait",
   icon: "./assets/icon.png",
   userInterfaceStyle: "automatic",
@@ -19,7 +19,7 @@ const config: ExpoConfig = {
   },
   android: {
     package: "ca.wopt.windsorprayertimes",
-    versionCode: 3,
+    versionCode: 4,
     adaptiveIcon: {
       foregroundImage: "./assets/adaptive-icon.png",
       backgroundColor: "#0b5b47"
