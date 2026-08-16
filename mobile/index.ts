6f39b74bdb17b02d68e70d371d7b7c482806f5ff
Hassoun911 <65694110+Hassoun911@users.noreply.github.com>
2026-08-15 23:52:14 -0400

    Expose email alerts from native app shell

--- mobile/index.ts
+++ mobile/index.ts
@@ -1,4 +1,4 @@
 import { registerRootComponent } from "expo";
-import App from "./App";
+import AppWithEmail from "./AppWithEmail";
 
-registerRootComponent(App);
+registerRootComponent(AppWithEmail);
