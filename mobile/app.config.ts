4d3889c287569538de1f5efdefff670607ac5f68
Hassoun911 <65694110+Hassoun911@users.noreply.github.com>
2026-08-17 02:13:11 -0400

    Bump Hassoun to v0.5.2 for persistent Quran audio

--- mobile/app.config.ts
+++ mobile/app.config.ts
@@ -5,7 +5,7 @@ const config: ExpoConfig = {
   slug: "wopt",
   owner: "hassoun911",
   scheme: "windsorprayer",
-  version: "0.5.1",
+  version: "0.5.2",
   orientation: "portrait",
   icon: "./assets/icon.png",
   userInterfaceStyle: "automatic",
@@ -20,7 +20,7 @@ const config: ExpoConfig = {
   },
   android: {
     package: "ca.wopt.windsorprayertimes",
-    versionCode: 23,
+    versionCode: 24,
     adaptiveIcon: {
       foregroundImage: "./assets/adaptive-icon.png",
       backgroundColor: "#003d33"
