294c0e9b340f68840fb16115984e2d9544864f60
Hassoun911 <65694110+Hassoun911@users.noreply.github.com>
2026-08-18 07:21:58 -0400

    Set Hassoun v0.6.0 release version

--- mobile/app.config.ts
+++ mobile/app.config.ts
@@ -5,7 +5,7 @@ const config: ExpoConfig = {
   slug: "wopt",
   owner: "hassoun911",
   scheme: "windsorprayer",
-  version: "0.5.9",
+  version: "0.6.0",
   orientation: "portrait",
   icon: "./assets/icon.png",
   userInterfaceStyle: "automatic",
@@ -20,7 +20,7 @@ const config: ExpoConfig = {
   },
   android: {
     package: "ca.wopt.windsorprayertimes",
-    versionCode: 31,
+    versionCode: 32,
     adaptiveIcon: {
       foregroundImage: "./assets/adaptive-icon.png",
       backgroundColor: "#003d33"
