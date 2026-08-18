7e5f38d89f268aa85a66c82987228ae8e8455e0e
Hassoun911 <65694110+Hassoun911@users.noreply.github.com>
2026-08-18 10:56:34 -0400

    Bump Hassoun to v0.6.3 smart alerts

--- mobile/app.config.ts
+++ mobile/app.config.ts
@@ -5,7 +5,7 @@ const config: ExpoConfig = {
   slug: "wopt",
   owner: "hassoun911",
   scheme: "windsorprayer",
-  version: "0.6.2",
+  version: "0.6.3",
   orientation: "portrait",
   icon: "./assets/icon.png",
   userInterfaceStyle: "automatic",
@@ -20,7 +20,7 @@ const config: ExpoConfig = {
   },
   android: {
     package: "ca.wopt.windsorprayertimes",
-    versionCode: 34,
+    versionCode: 35,
     adaptiveIcon: {
       foregroundImage: "./assets/adaptive-icon.png",
       backgroundColor: "#003d33"
