0515e5eb30ebfa7a2c703050cbc94191e9575c10
Hassoun911 <65694110+Hassoun911@users.noreply.github.com>
2026-08-20 01:10:48 -0400

    Add standalone Admin CRM entry page

---
+++ admin-crm/app/page.tsx
@@ -0,0 +1,11 @@
+import CrmDashboard from "../../pwa/app/admin/CrmDashboard";
+import PasswordResetLink from "../../pwa/app/admin/PasswordResetLink";
+
+export default function AdminCrmPage() {
+  return (
+    <>
+      <CrmDashboard />
+      <PasswordResetLink />
+    </>
+  );
+}
