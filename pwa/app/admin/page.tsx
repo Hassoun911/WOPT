7a6f87fc5a6c998f0f65a1385744fffb9a04e761
Hassoun911 <65694110+Hassoun911@users.noreply.github.com>
2026-08-19 23:53:41 -0400

    Show forgot password link on Admin CRM login

--- pwa/app/admin/page.tsx
+++ pwa/app/admin/page.tsx
@@ -1,5 +1,11 @@
 import CrmDashboard from "./CrmDashboard";
+import PasswordResetLink from "./PasswordResetLink";
 
 export default function AdminPage() {
-  return <CrmDashboard />;
+  return (
+    <>
+      <CrmDashboard />
+      <PasswordResetLink />
+    </>
+  );
 }
