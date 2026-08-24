2ff65eedd4bbe1de53a74b99f5200ee79b28ebce
Hassoun911 <65694110+Hassoun911@users.noreply.github.com>
2026-08-24 15:22:29 -0400

    Bump iOS 1.0.8 build number

--- ios/app.config.ts
+++ ios/app.config.ts
@@ -12,7 +12,7 @@ const config: ExpoConfig = {
   ios: {
     supportsTablet: false,
     bundleIdentifier: "ca.wopt.windsorprayertimes",
-    buildNumber: "20260823",
+    buildNumber: "20260824",
     infoPlist: {
       ITSAppUsesNonExemptEncryption: false,
       UIBackgroundModes: ["remote-notification", "audio"],
