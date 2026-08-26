a5db158c1d9c6530f9c9a60cea24ee131ffdd2b2
Hassoun911 <65694110+Hassoun911@users.noreply.github.com>
2026-08-25 23:25:22 -0400

    Stage repaired Hassoun admin route

---
+++ pwa/app/admin/page.new.tsx
@@ -0,0 +1,11 @@
+import CrmDashboard from "./CrmDashboard";
+import PasswordResetLink from "./PasswordResetLink";
+
+export default function AdminPage() {
+  return (
+    <>
+      <CrmDashboard />
+      <PasswordResetLink />
+    </>
+  );
+}
