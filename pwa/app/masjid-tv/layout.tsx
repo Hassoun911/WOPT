f5294c84ce84572373e469315cede478c608e1de
Hassoun911 <65694110+Hassoun911@users.noreply.github.com>
2026-08-30 01:50:09 -0400

    Load replica one validated header fixes

--- pwa/app/masjid-tv/layout.tsx
+++ pwa/app/masjid-tv/layout.tsx
@@ -6,6 +6,7 @@ import "./masjid-tv-polish.css";
 import "./reference-layouts.css";
 import "./jumuah-exact.css";
 import "./replica-one.css";
+import "./replica-one-header-fix.css";
 
 export default function MasjidTvLayout({ children }: { children: ReactNode }) {
   return <>{children}<LogoUploadEnhancer /><TvModeChromeSuppressor /><ReplicaOneEnhancer /></>;
