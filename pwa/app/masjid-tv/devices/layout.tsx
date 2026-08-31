218d6b51c72379948214f1822232f260064b3f5c
Hassoun911 <65694110+Hassoun911@users.noreply.github.com>
2026-08-31 00:16:57 -0400

    Mount logo upload and bulk schedule importer

--- pwa/app/masjid-tv/devices/layout.tsx
+++ pwa/app/masjid-tv/devices/layout.tsx
@@ -1,7 +1,8 @@
 import type { ReactNode } from "react";
 import TickerControl from "./TickerControl";
 import SmartDisplayEditorEnhancer from "./SmartDisplayEditorEnhancer";
+import LocalLogoAndScheduleImport from "./LocalLogoAndScheduleImport";
 
 export default function DevicesLayout({children}:{children:ReactNode}){
-  return <><TickerControl/>{children}<SmartDisplayEditorEnhancer /></>;
+  return <><TickerControl/>{children}<SmartDisplayEditorEnhancer /><LocalLogoAndScheduleImport /></>;
 }
