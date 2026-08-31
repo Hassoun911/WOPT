0dbbd569526d25314e5535225c5748196567cfe4
Hassoun911 <65694110+Hassoun911@users.noreply.github.com>
2026-08-30 22:26:57 -0400

    Enable TV Studio pairing and remote scrolling

--- pwa/app/masjid-tv/layout.tsx
+++ pwa/app/masjid-tv/layout.tsx
@@ -3,6 +3,7 @@ import LogoUploadEnhancer from "./LogoUploadEnhancer";
 import TvModeChromeSuppressor from "./TvModeChromeSuppressor";
 import PixelReplicaEnhancer from "./PixelReplicaEnhancer";
 import StudioPreviewEnhancer from "./StudioPreviewEnhancer";
+import StudioDevicePairingEnhancer from "./StudioDevicePairingEnhancer";
 import MasjidTvWakeLock from "./MasjidTvWakeLock";
 import "./masjid-tv-polish.css";
 import "./reference-layouts.css";
@@ -11,5 +12,5 @@ import "./pixel-replica.css";
 import "./studio-previews.css";
 
 export default function MasjidTvLayout({ children }: { children: ReactNode }) {
-  return <>{children}<MasjidTvWakeLock /><LogoUploadEnhancer /><TvModeChromeSuppressor /><PixelReplicaEnhancer /><StudioPreviewEnhancer /></>;
+  return <>{children}<MasjidTvWakeLock /><LogoUploadEnhancer /><TvModeChromeSuppressor /><PixelReplicaEnhancer /><StudioPreviewEnhancer /><StudioDevicePairingEnhancer /></>;
 }
