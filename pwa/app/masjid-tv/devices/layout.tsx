33b04ac8fe25390ea151eb3b0b18d62b6f01b9c5
Hassoun911 <65694110+Hassoun911@users.noreply.github.com>
2026-08-31 00:04:37 -0400

    Add contextual smart controls to My Displays

--- pwa/app/masjid-tv/devices/layout.tsx
+++ pwa/app/masjid-tv/devices/layout.tsx
@@ -1,6 +1,7 @@
 import type { ReactNode } from "react";
 import TickerControl from "./TickerControl";
+import SmartDisplayEditorEnhancer from "./SmartDisplayEditorEnhancer";
 
 export default function DevicesLayout({children}:{children:ReactNode}){
-  return <><TickerControl/>{children}</>;
+  return <><TickerControl/>{children}<SmartDisplayEditorEnhancer /></>;
 }
