bc051df84467f4826544e74337e980332949c1c9
Hassoun911 <65694110+Hassoun911@users.noreply.github.com>
2026-09-01 00:55:20 -0400

    Use unified Smart Grand V2 display renderer

--- pwa/app/masjid-tv/layout.tsx
+++ pwa/app/masjid-tv/layout.tsx
@@ -1,10 +1,10 @@
 import type { ReactNode } from "react";
 import LogoUploadEnhancer from "./LogoUploadEnhancer";
 import DisplayArtSettingsEnhancer from "./DisplayArtSettingsEnhancer";
+import SmartGrandV2Enhancer from "./SmartGrandV2Enhancer";
+import SmartMasjidSettingsEnhancer from "./SmartMasjidSettingsEnhancer";
 import TvModeChromeSuppressor from "./TvModeChromeSuppressor";
-import PixelReplicaEnhancer from "./PixelReplicaEnhancer";
 import StudioPreviewEnhancer from "./StudioPreviewEnhancer";
-import StudioGrandMirrorEnhancer from "./StudioGrandMirrorEnhancer";
 import StudioDevicePairingEnhancer from "./StudioDevicePairingEnhancer";
 import MasjidTvWakeLock from "./MasjidTvWakeLock";
 import TvTickerFooter from "./TvTickerFooter";
@@ -18,5 +18,5 @@ import "./pixel-replica.css";
 import "./studio-previews.css";
 
 export default function MasjidTvLayout({ children }: { children: ReactNode }) {
-  return <>{children}<MasjidTvWakeLock /><LogoUploadEnhancer /><DisplayArtSettingsEnhancer /><TvModeChromeSuppressor /><PixelReplicaEnhancer /><StudioPreviewEnhancer /><StudioGrandMirrorEnhancer /><StudioDevicePairingEnhancer /><TvTickerFooter /><SmartVerseDisplayEnhancer /><TvIslamicCalendarEnhancer /><DonationQrEnhancer /></>;
+  return <>{children}<MasjidTvWakeLock /><LogoUploadEnhancer /><DisplayArtSettingsEnhancer /><SmartMasjidSettingsEnhancer /><TvModeChromeSuppressor /><SmartGrandV2Enhancer /><StudioPreviewEnhancer /><StudioDevicePairingEnhancer /><TvTickerFooter /><SmartVerseDisplayEnhancer /><TvIslamicCalendarEnhancer /><DonationQrEnhancer /></>;
 }
