72a0fe132b05e5fd1cad8746b7c70b47daf9b1c0
Hassoun911 <65694110+Hassoun911@users.noreply.github.com>
2026-08-31 22:37:18 -0400

    Add schedule intent and yearly calendar controls

--- pwa/app/masjid-tv/devices/layout.tsx
+++ pwa/app/masjid-tv/devices/layout.tsx
@@ -2,11 +2,13 @@ import type { ReactNode } from "react";
 import TickerControl from "./TickerControl";
 import SmartDisplayEditorEnhancer from "./SmartDisplayEditorEnhancer";
 import LocalLogoAndScheduleImport from "./LocalLogoAndScheduleImport";
+import ScheduleIntentEnhancer from "./ScheduleIntentEnhancer";
+import YearScheduleManager from "./YearScheduleManager";
 import PreviewLogoSync from "./PreviewLogoSync";
 import DevicesScrollFix from "./DevicesScrollFix";
 import IslamicCalendarControls from "./IslamicCalendarControls";
 import PreviewFooterSync from "./PreviewFooterSync";
 
 export default function DevicesLayout({children}:{children:ReactNode}){
-  return <><DevicesScrollFix/><TickerControl/>{children}<SmartDisplayEditorEnhancer /><LocalLogoAndScheduleImport /><PreviewLogoSync /><IslamicCalendarControls /><PreviewFooterSync /></>;
+  return <><DevicesScrollFix/><TickerControl/>{children}<SmartDisplayEditorEnhancer /><LocalLogoAndScheduleImport /><ScheduleIntentEnhancer /><YearScheduleManager /><PreviewLogoSync /><IslamicCalendarControls /><PreviewFooterSync /></>;
 }
