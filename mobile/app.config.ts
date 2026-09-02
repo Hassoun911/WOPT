6695189705e657fe3ff85e847d818a4d251bc5f7
Hassoun911 <65694110+Hassoun911@users.noreply.github.com>
2026-09-02 13:47:25 -0700

    Bump Hassoun Android to v1.0.18 code 58

--- mobile/app.config.ts
+++ mobile/app.config.ts
@@ -5,7 +5,7 @@ const config: ExpoConfig = {
   slug: "wopt",
   owner: "hassoun911",
   scheme: "windsorprayer",
-  version: "1.0.17",
+  version: "1.0.18",
   orientation: "portrait",
   icon: "./assets/icon.png",
   userInterfaceStyle: "automatic",
@@ -22,7 +22,7 @@ const config: ExpoConfig = {
   },
   android: {
     package: "ca.wopt.windsorprayertimes",
-    versionCode: 57,
+    versionCode: 58,
     icon: "./assets/icon.png",
     adaptiveIcon: {
       foregroundImage: "./assets/adaptive-icon.png",
