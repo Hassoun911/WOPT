de981d884bba6aecbee7e4f5a5e7303db3fd768f
Hassoun911 <65694110+Hassoun911@users.noreply.github.com>
2026-09-02 12:48:13 -0700

    Bump Hassoun Android to v1.0.17 code 57

--- mobile/app.config.ts
+++ mobile/app.config.ts
@@ -5,7 +5,7 @@ const config: ExpoConfig = {
   slug: "wopt",
   owner: "hassoun911",
   scheme: "windsorprayer",
-  version: "1.0.16",
+  version: "1.0.17",
   orientation: "portrait",
   icon: "./assets/icon.png",
   userInterfaceStyle: "automatic",
@@ -22,7 +22,7 @@ const config: ExpoConfig = {
   },
   android: {
     package: "ca.wopt.windsorprayertimes",
-    versionCode: 56,
+    versionCode: 57,
     icon: "./assets/icon.png",
     adaptiveIcon: {
       foregroundImage: "./assets/adaptive-icon.png",
