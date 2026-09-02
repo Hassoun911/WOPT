fbd02628420328d4bbbca2f5b75b5c14f04d59fa
Hassoun911 <65694110+Hassoun911@users.noreply.github.com>
2026-09-01 20:55:34 -0700

    Wire mosque timetable sync into Masjid TV

--- pwa/app/masjid-tv/layout.tsx
+++ pwa/app/masjid-tv/layout.tsx
@@ -8,6 +8,7 @@ import TvModeChromeSuppressor from "./TvModeChromeSuppressor";
 import TvDisplayVisibilityGuard from "./TvDisplayVisibilityGuard";
 import TvClockSettingsBridge from "./TvClockSettingsBridge";
 import PrayerSourceResolverEnhancer from "./PrayerSourceResolverEnhancer";
+import MosqueTimetableSyncEnhancer from "./MosqueTimetableSyncEnhancer";
 import DirectTvAdvancedSettingsEnhancer from "./DirectTvAdvancedSettingsEnhancer";
 import StudioPreviewEnhancer from "./StudioPreviewEnhancer";
 import StudioDevicePairingEnhancer from "./StudioDevicePairingEnhancer";
@@ -23,5 +24,5 @@ import "./pixel-replica.css";
 import "./studio-previews.css";
 
 export default function MasjidTvLayout({ children }: { children: ReactNode }) {
-  return <>{children}<MasjidTvWakeLock /><LogoUploadEnhancer /><DisplayArtSettingsEnhancer /><SmartMasjidSettingsEnhancer /><TvModeChromeSuppressor /><TvDisplayVisibilityGuard /><PrayerSourceResolverEnhancer /><SmartGrandV2Enhancer /><TvClockSettingsBridge /><DirectTvAdvancedSettingsEnhancer /><AdminPreviewStabilityEnhancer /><StudioPreviewEnhancer /><StudioDevicePairingEnhancer /><TvTickerFooter /><SmartVerseDisplayEnhancer /><TvIslamicCalendarEnhancer /><DonationQrEnhancer /></>;
+  return <>{children}<MasjidTvWakeLock /><LogoUploadEnhancer /><DisplayArtSettingsEnhancer /><SmartMasjidSettingsEnhancer /><TvModeChromeSuppressor /><TvDisplayVisibilityGuard /><PrayerSourceResolverEnhancer /><MosqueTimetableSyncEnhancer /><SmartGrandV2Enhancer /><TvClockSettingsBridge /><DirectTvAdvancedSettingsEnhancer /><AdminPreviewStabilityEnhancer /><StudioPreviewEnhancer /><StudioDevicePairingEnhancer /><TvTickerFooter /><SmartVerseDisplayEnhancer /><TvIslamicCalendarEnhancer /><DonationQrEnhancer /></>;
 }
