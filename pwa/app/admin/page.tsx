e45725d853ea7ee8cdfee9dc976c36429e855d89
Hassoun911 <65694110+Hassoun911@users.noreply.github.com>
2026-09-14 09:25:43 -0700

    Add location coverage to admin home

--- pwa/app/admin/page.tsx
+++ pwa/app/admin/page.tsx
@@ -1,10 +1,12 @@
 import CrmDashboard from "./CrmDashboard";
+import LocationCoverageCard from "./LocationCoverageCard";
 import PasswordResetLink from "./PasswordResetLink";
 
 export default function AdminPage() {
   return (
     <>
       <CrmDashboard />
+      <LocationCoverageCard />
       <PasswordResetLink />
     </>
   );
